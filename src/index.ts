a5bfc6a7044aafc3ffbc04c89f55a2d3baa2fa39
Melvin Gaye <melvind.gaye@gmail.com>
2022-03-21 20:50:27 -0400

    docs: should run lint-staged test three

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,5 @@ const server = http.createServer((_req, res) => {
 server.listen(3000, () => {
 	console.log('app running two three');
 	console.log('test change here');
+	console.log('test change here');
 });
